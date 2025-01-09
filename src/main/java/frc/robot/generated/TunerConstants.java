3f291b7c14f1d68e11e102c0e303e0389f547880
silasnerad <silasnerad @gmail.com>
2025-01-08 20:25:14 -0600

    Change values in TunerConstants Match the gear rarios of the standard MK3 swerve modules. Match M/s speed for MK3 standards: Falcon Unadjusted Free Speed.

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -80,14 +80,16 @@ public class TunerConstants {
 
   // Theoretical free speed (m/s) at 12 V applied output;
   // This needs to be tuned to your individual robot
-  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.73);
+  //mk3 Falcon unadjusted speed (13.6 ft/s)
+  public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.15); 
 
   // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
   // This may need to be tuned to your individual robot
-  private static final double kCoupleRatio = 3.5714285714285716;
+  private static final double kCoupleRatio = 3.5714285714285716; //TODO Validate, was 0 on old robot
+  
 
-  private static final double kDriveGearRatio = 6.746031746031747;
-  private static final double kSteerGearRatio = 12.8;
+  private static final double kDriveGearRatio = 8.16; //Standard mk3 module
+  private static final double kSteerGearRatio = 12.8; //standard mk3 module
   private static final Distance kWheelRadius = Inches.of(2);
 
   private static final boolean kInvertLeftSide = false;
