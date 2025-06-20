01067f47cf8dbd5b4a5fcfcd1fbae10d6dab523d
Logan Kitzman <rkitzman4@gmail.com>
2025-06-19 20:30:20 -0500

    updated autos

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -40,7 +40,8 @@ public class VisionConstants {
 
   // Standard deviation baselines(0.01), for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 1.0; // Meters, 0.01 was original value was hyper sensitive.
+  public static double linearStdDevBaseline =
+      1.0; // Meters, 0.01 was original value was hyper sensitive.
   public static double angularStdDevBaseline = 0.06; // Radians
 
   // Standard deviation multipliers for each camera
